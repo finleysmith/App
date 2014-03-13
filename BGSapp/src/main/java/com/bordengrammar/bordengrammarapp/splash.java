d6de0043aa285fd523a20fd115d2acc730bbc1ec
epicfinley <epicfinley@gmail.com>
2014-03-13 18:08:36 +0000

    Added a splash screen
    Signed-off-by: epicfinley <epicfinley@gmail.com>

---
+++ BGSapp/src/main/java/com/bordengrammar/bordengrammarapp/splash.java
@@ -0,0 +1,36 @@
+package com.bordengrammar.bordengrammarapp;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(splash.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
