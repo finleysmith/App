ff67e13e973b16491bd9065f192b57ac9bb92208
epicfinley <epicfinley@gmail.com>
2014-03-27 16:43:24 +0000

    redone twitter thing

--- BGSapp/src/main/java/com/bordengrammar/bordengrammarapp/HomeFragment.java
+++ BGSapp/src/main/java/com/bordengrammar/bordengrammarapp/HomeFragment.java
@@ -27,61 +27,35 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
 
 	    View myInflatedView = inflater.inflate(R.layout.fragment_home, container,false);
-	    Thread thread = new Thread(new Runnable(){
-		    @Override
-		    public void run() {
-			    try {
-
-				    ConfigurationBuilder cb = new ConfigurationBuilder();
-				    cb.setDebugEnabled(true)
-						    .setOAuthConsumerKey("Toqp03fcUErG5P8e9nhfsw")
-						    .setOAuthConsumerSecret(
-								    "SEqktstO9h7SqSm7zmcuWlH3bOtElJm1Ds2TFSwFBc")
-						    .setOAuthAccessToken(
-								    "2245935685-U5LMfl4oEcOv6Khw58JZqRdcH2PlABEeUP2JeXj")
-						    .setOAuthAccessTokenSecret(
-								    "uFcGRpCx8aGXdv3AiAkfVImnoLrlNNCUnZ2UtE76Zbnpa");
-				    TwitterFactory tf = new TwitterFactory(cb.build());
-				    Twitter twitter = tf.getInstance();
-				    try {
-					    List<twitter4j.Status> statuses;
-					    String user;
-					    user = "epicfinley";
-					    statuses = twitter.getUserTimeline(user);
-					    twitter4j.Status status = statuses.get(0);
-					    TWEET = status.getText();
 
 
-				    } catch (TwitterException te) {
-					    te.printStackTrace();
-				    }
-			    } catch (Exception e) {
-				    e.printStackTrace();
-			    }
-		    }
-	    });
-
-	    TextView t;
-	    assert myInflatedView != null;
-	    t = (TextView) myInflatedView.findViewById(R.id.textView3);
-	    if(TWEET != null && !TWEET.isEmpty()){
-		    //t.setVisibility(View.VISIBLE);
-		    t.setText(TWEET);
-		    savePrefs("TWEETY", TWEET);
-	    } else {
-		    SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
-		    String name = sp.getString("TWEETY", "First time? Explore all the tabs!");
-		    if (name != null && !name.isEmpty()) {
-			    t.setText(name);
-
-		    } else {
-			    t.setVisibility(View.INVISIBLE);
-		    }
+	    ConfigurationBuilder cb = new ConfigurationBuilder();
+	    cb.setDebugEnabled(true)
+			    .setOAuthConsumerKey("Toqp03fcUErG5P8e9nhfsw")
+			    .setOAuthConsumerSecret(
+					    "SEqktstO9h7SqSm7zmcuWlH3bOtElJm1Ds2TFSwFBc")
+			    .setOAuthAccessToken(
+					    "2245935685-U5LMfl4oEcOv6Khw58JZqRdcH2PlABEeUP2JeXj")
+			    .setOAuthAccessTokenSecret(
+					    "uFcGRpCx8aGXdv3AiAkfVImnoLrlNNCUnZ2UtE76Zbnpa");
+	    TwitterFactory tf = new TwitterFactory(cb.build());
+	    Twitter twitter = tf.getInstance();
+	    List<twitter4j.Status> statuses = null;
+	    String user;
+	    user = "epicfinley";
+	    try {
+		    statuses = twitter.getUserTimeline(user);
+	    } catch (TwitterException e) {
+		    e.printStackTrace();
 	    }
-	    thread.start();
+	    twitter4j.Status status = statuses.get(0);
+	    TextView t;
+	    t = (TextView) myInflatedView.findViewById(R.id.textView3);
+	    t.setText('"' + status.getText() + '"');
+
+
 	    return myInflatedView;
     }
-
 
 	private void savePrefs(String key, String value) {
 		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
