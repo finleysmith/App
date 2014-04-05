2087f797b69c67e7a2214b71892bebe67ce17407
epicfinley <epicfinley@gmail.com>
2014-04-05 17:48:24 +0100

    Updated Copyright
    Signed-off-by: epicfinley <epicfinley@gmail.com>

--- BGSapp/src/main/java/com/bordengrammar/bordengrammarapp/HomeFragment.java
+++ BGSapp/src/main/java/com/bordengrammar/bordengrammarapp/HomeFragment.java
@@ -1,3 +1,20 @@
+// Copyright (C) 2014.  Finley Smith
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// Also add information on how to contact you by electronic and paper mail.
+
 package com.bordengrammar.bordengrammarapp;
 
 
@@ -57,7 +74,7 @@ public class HomeFragment extends Fragment {
 
 		// Move the camera instantly to borden with a zoom of 15.
 		map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-		map.setTrafficEnabled(true);
+		map.setMyLocationEnabled(true);
 		map.moveCamera(CameraUpdateFactory.newLatLngZoom(BORDEN, 15));
 
 		// Zoom in, animating the camera.
@@ -70,7 +87,8 @@ public class HomeFragment extends Fragment {
 		t.setText('"' + readPrefs("twitter") + '"');
 		TextView t1;
 		t1 = (TextView) myInflatedView.findViewById(R.id.date);
-		t1.setText("bordengrammar | " + readPrefs("twittertime"));
+		String source2 = "&#64;";
+		t1.setText(Html.fromHtml(source2) + "bordengrammar | " + readPrefs("twittertime"));
 
 		//Typeface normal = Typeface.createFromAsset(getActivity().getResources().getAssets(), "fonts/normal.ttf");
 		//Typeface light = Typeface.createFromAsset(getActivity().getResources().getAssets(), "fonts/light.tff");
