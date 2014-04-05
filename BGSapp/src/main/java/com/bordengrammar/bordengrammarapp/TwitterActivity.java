3de0e53e38cb5d6e2351b204a49853f83393bf0e
epicfinley <epicfinley@gmail.com>
2014-04-05 18:00:15 +0100

    added twitteractivity
    Signed-off-by: epicfinley <epicfinley@gmail.com>

---
+++ BGSapp/src/main/java/com/bordengrammar/bordengrammarapp/TwitterActivity.java
@@ -0,0 +1,172 @@
+// Copyright (C) 2014.  Finley Smith
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// Also add information on how to contact you by electronic and paper mail.
+
+/*
+* Created By Finley Smith on 5/4/14
+ */
+
+package com.bordengrammar.bordengrammarapp;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.suredigit.inappfeedback.FeedbackDialog;
+import com.suredigit.inappfeedback.FeedbackSettings;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+
+public class TwitterActivity extends Activity {
+
+	public String TAG = "MainActivity";
+	private FeedbackDialog feedBack;
+	private ActionBar actionBar;
+	public static String PACKAGE_NAME;
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+		FeedbackSettings feedbackSettings = new FeedbackSettings();
+		feedbackSettings.setCancelButtonText("Cancel");
+		feedbackSettings.setSendButtonText("Send");
+		feedbackSettings.setText("Send feedback to improve the app");
+		feedbackSettings.setTitle("Feedback");
+		feedbackSettings.setToast("We value your feedback");
+		feedBack = new FeedbackDialog(this, "AF-186C1F794D93-1A", feedbackSettings);
+		actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		PACKAGE_NAME = getApplicationContext().getPackageName();
+
+
+		//Now lets get down into the twitter
+
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+				.setOAuthConsumerKey("Toqp03fcUErG5P8e9nhfsw")
+				.setOAuthConsumerSecret(
+						"SEqktstO9h7SqSm7zmcuWlH3bOtElJm1Ds2TFSwFBc")
+				.setOAuthAccessToken(
+						"2245935685-U5LMfl4oEcOv6Khw58JZqRdcH2PlABEeUP2JeXj")
+				.setOAuthAccessTokenSecret(
+						"uFcGRpCx8aGXdv3AiAkfVImnoLrlNNCUnZ2UtE76Zbnpa");
+
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
+		List<Status> statuses = null;
+		String user;
+		user = "epicfinley";
+		try {
+			statuses = twitter.getUserTimeline(user);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+
+		assert statuses != null;
+		twitter4j.Status status1 = statuses.get(0);
+		Format formatter = new SimpleDateFormat("MMM d, K:mm");
+		savePrefs("twitter", status1.getText());
+		savePrefs("twittertime", formatter.format(status1.getCreatedAt()));
+		twitter4j.Status status2 = statuses.get(1);
+		twitter4j.Status status3 = statuses.get(2);
+		twitter4j.Status status4 = statuses.get(3);
+		twitter4j.Status status5 = statuses.get(4);
+		twitter4j.Status status6 = statuses.get(5);
+		twitter4j.Status status7 = statuses.get(6);
+		twitter4j.Status status8 = statuses.get(7);
+		twitter4j.Status status9 = statuses.get(8);
+		twitter4j.Status satus10 = statuses.get(9);
+
+
+
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) { //after the menu has been inflated
+		switch (item.getItemId()) { //get the id fors the menus from our menu.xml
+			case android.R.id.home: //if it is home
+				Intent l = new Intent(TwitterActivity.this, MainActivity.class);
+				startActivity(l);
+				return true; //break it so it does not go onto next case
+			case R.id.facebook: //if it is facebook button
+				logIt("Facebook button clicked"); //logit
+				Intent faceBrowserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/BordenGrammarSchool"));  //Create intent variable
+				startActivity(faceBrowserIntent); //Start that intent
+				return true;
+			case R.id.website: //if they clicked they
+				Log.i(TAG, "Website Clicked");
+				Intent websiteBrowserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://website.bordengrammar.kent.sch.uk/")); //Same as above
+				startActivity(websiteBrowserIntent);
+				return true;
+			case R.id.action_settings: //action settings actually about, can't change it now
+				logIt("clicked about");
+				Intent i = new Intent(TwitterActivity.this, AboutActivity.class);
+				startActivity(i);
+
+				return true;
+			case R.id.action_feedback: //if they clciked send feedback
+				logIt("feedback");
+				feedBack.show(); //show the feedback that we declared
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+
+	}
+
+	public void logIt(String it) {
+		Log.i(TAG, it);
+	}
+	private void savePrefs(String key, String value) {
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+		SharedPreferences.Editor edit = sp.edit();
+		edit.putString(key, value);
+		edit.commit();
+	}
+	public String readPrefs(String key) {
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+		return sp.getString(key, "An error occured whilst fetching twitter feed");
+	}
+
+
+}
